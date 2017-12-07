5121180590af3cdbaf2abbce10a61c1573546adb
xiaoningboke <sunxiaoning@aliyun.com>
2017-12-07 11:40:38 +0800

    管理员

--- Application/Home/Controller/AdminController.class.php
+++ Application/Home/Controller/AdminController.class.php
@@ -15,7 +15,7 @@ class AdminController extends Controller {
     	$jsonString = '{"code":0,"msg":"","count":'.$code.',"data":[{';
     	//var_dump($jsonData);
     	foreach ($studentDate as $value) {
-    			$jsonData = $jsonData.'"id":'.$value[id].'-----';
+    			$jsonData = $jsonData.'"id":'.$value[id].',"number:"'.$value[number].',"name:"'.$value[name].',"sex:"'.$value[sex].',"email:"'.$value[email];
     	}
     	var_dump($jsonData);
     	//var_dump($studentDate);
